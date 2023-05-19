494eb40a33a41d79166bb1c91808452804746b0b
zouxianyu <2979121738@qq.com>
2023-05-19 14:41:17 +0800

    初始化项目

---
+++ src/main/java/com/se/english_exam/EnglishExamApplication.java
@@ -0,0 +1,13 @@
+package com.se.english_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/se/english_exam/EnglishExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.english_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
