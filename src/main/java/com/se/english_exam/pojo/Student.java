eef85be950cff9452fe935252593438ed275f7dc
zouxianyu <2979121738@qq.com>
2023-05-20 20:55:28 +0800

    修改学生实体类，更改报名信息字段名称，修改身份证号字段类型

--- src/main/java/com/se/english_exam/pojo/Student.java
+++ src/main/java/com/se/english_exam/pojo/Student.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 public class Student {
     private Integer id;
     private String name;
-    private Integer idCard;
+    private String idCard;
     private String password;
-    private Boolean signUp;
+    private Boolean enroll;
 }
