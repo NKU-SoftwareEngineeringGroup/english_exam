6275e6b865dfdd97aeec629b5f156a16cbb05cdc
zouxianyu <2979121738@qq.com>
2023-05-20 20:54:31 +0800

    在程序入口加入MapperScan注解，用于支持mybatis-plus

--- src/main/java/com/se/english_exam/EnglishExamApplication.java
+++ src/main/java/com/se/english_exam/EnglishExamApplication.java
@@ -1,9 +1,11 @@
 package com.se.english_exam;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.se.english_exam.mapper")
 public class EnglishExamApplication {
 
     public static void main(String[] args) {
